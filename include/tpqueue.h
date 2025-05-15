5eb5fc6c957178db60a43687615edd5212aa0b6d
liguhska <masharostenko@mail.ru>
2025-05-15 20:17:31 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,64 +5,63 @@
 #include <iostream>
 #include <stdexcept>
 struct SYM {
-	char ch;
-	int prior;
+  char ch;
+  int prior;
 };
 
 template<typename T>
 class TPQueue {
 private:
-	struct Node {
-		T data;
-		Node* next;
-		Node(T value) : data(value), next(nullptr) {}
-	};
+  struct Node {
+    T data;
+    Node* next;
+    Node(T value) : data(value), next(nullptr) {}
+  };
 
-	Node* head;
+  Node* head;
 
 public:
-	TPQueue() : head(nullptr) {}
-	~TPQueue() {
-		while (head != nullptr) {
-			Node* temp = head;
-			head = head->next;
-			delete temp;
-		}
-	}
+  TPQueue() : head(nullptr) {}
+  ~TPQueue() {
+    while (head != nullptr) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
 
-	bool isEmpty() const {
-		return head == nullptr;
-	}
+  bool isEmpty() const {
+    return head == nullptr;
+  }
 
-	void push(const T& val) {
-		Node* newNode = new Node(val);
-		if (isEmpty() || head->data.prior < val.prior) {
-			newNode->next = head;
-			head = newNode;
-			return;
-		}
+  void push(const T& val) {
+    Node* newNode = new Node(val);
+    if (isEmpty() || head->data.prior < val.prior) {
+      newNode->next = head;
+      head = newNode;
+      return;
+    }
 
-		Node* cur = head;
-		while (cur->next != nullptr && cur->next->data.prior >= val.prior) {
-			cur = cur->next;
-		}
-		newNode->next = cur->next;
-		cur->next = newNode;
-	}
+    Node* cur = head;
+    while (cur->next != nullptr && cur->next->data.prior >= val.prior) {
+      cur = cur->next;
+    }
+    newNode->next = cur->next;
+    cur->next = newNode;
+  }
 
-	T pop() {
-		if (isEmpty()) {
-			throw std::out_of_range("Queue is empty");
-		}
+  T pop() {
+    if (isEmpty()) {
+      throw std::out_of_range("Queue is empty");
+    }
 
-		Node* temp = head;
-		T val = head->data;
-		head = head->next;
-		delete temp;
+    Node* temp = head;
+    T val = head->data;
+    head = head->next;
+    delete temp;
 
-		return val;
-	}
+    return val;
+  }
 };
-
 
 #endif  // INCLUDE_TPQUEUE_H_
